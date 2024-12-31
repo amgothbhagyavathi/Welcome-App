98acf16ce75639ee1761378fc858ccc008d59289
amgothbhagyavathi <amgothbhagyavathi314@gmail.com>
2024-12-31 02:04:19 +0000

    Updated Code

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  onSubscribe = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const textButton = this.getButtonText()
+    return (
+      <div className="constainer">
+        <h1 className="heading">Welcome</h1>
+        <p className="para">Thank you! Happy Learning</p>
+        <button type="button" onClick={this.onSubscribe}>
+          {textButton}
+        </button>
+      </div>
+    )
+  }
+}
+export default Welcome
